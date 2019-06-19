c873895957efe9284dd653ce4029af5e0202b553
Juliakg <50931662+Juliakg@users.noreply.github.com>
2019-06-19 18:21:43 -0400

    color changes plus text
    Changed footer to red. Increased the size of my name in head. Changes About Me text.

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Hello " + name);
+
+var name = "Julia";
+alert("Hello " + name);
+
+name = "Katie";
+alert("Hello " + name);
---
+++ script.js
@@ -0,0 +1,6 @@
+var x = "Julia";
+alert("Hello " + name);
+function signiture(name) {
+  alert(name + " K Gaines");
+}
+signiture(x);
