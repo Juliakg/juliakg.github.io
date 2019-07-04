111d1c184eba333520a7f6849ad4b160930f4202
Juliakg <50931662+Juliakg@users.noreply.github.com>
2019-07-04 14:56:18 -0400

    july 4 update
    Many changes to Index  and Cohorts. Increased the size of my picture and added a return to top link. Added 1st draft of some of my classmates that have provided their link.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-alert("Hello " + name);
-
-var name = "Julia";
-alert("Hello " + name);
-
-name = "Katie";
-alert("Hello " + name);
+// alert("Hello " + name);
+//
+// var name = "Julia";
+// alert("Hello " + name);
+//
+// name = "Katie";
+// alert("Hello " + name);
